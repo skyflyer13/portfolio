63cb0635b1e58689345000670321ce605f3d1fad
Grega Ule <grega.ule@gmail.com>
2020-02-12 16:31:05 +0100

    Menu Overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,5 +26,8 @@ function toggleMenu() {
     menuNav.classList.remove('show');
     menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
+
+    // Set Menu State
+    showMenu = false;
   }
 }
